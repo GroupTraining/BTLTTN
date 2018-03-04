a91c9975b6bb3bb9cc6ea4b751649e79e905574b
a365ngay <35994656+a365ngay@users.noreply.github.com>
2018-03-04 17:48:54 +0700

    Tìm kiếm 1.1

---
+++ QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/GUI/Timkiem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhanSu.GUI
+{
+    public partial class Timkiem : Form
+    {
+        public Timkiem()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Main main = new Main();
+
+            main.ShowDialog();
+        }
+    }
+}
