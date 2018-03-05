a41e3eb4e7ab40f2d35c998743aa4b63fd2245ab
giangttvm <36163538+giangttvm@users.noreply.github.com>
2018-03-05 22:24:05 +0700

    add form update

---
+++ QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/GUI/Add.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace QuanLyNhanSu.GUI
+{
+    partial class Add
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtNS = new System.Windows.Forms.TextBox();
+            this.txtLuong = new System.Windows.Forms.TextBox();
+            this.txtMaNGS = new System.Windows.Forms.TextBox();
+            this.txtMaPB = new System.Windows.Forms.TextBox();
+            this.btnTHÊM = new System.Windows.Forms.Button();
+            this.dgvNhanVien = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDuAn = new System.Windows.Forms.TextBox();
+            this.rabtn_nam = new System.Windows.Forms.RadioButton();
+            this.rabtn_nu = new System.Windows.Forms.RadioButton();
+            this.MaNV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNhanVien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MaNV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(408, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NS";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Tag = "Lương";
+            this.label5.Text = "Luong";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(203, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "MaNGS";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(394, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "MaPB";
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(62, 19);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(100, 20);
+            this.txtMaNV.TabIndex = 7;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(249, 19);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(100, 20);
+            this.txtHoTen.TabIndex = 8;
+            // 
+            // txtNS
+            // 
+            this.txtNS.Location = new System.Drawing.Point(436, 19);
+            this.txtNS.Name = "txtNS";
+            this.txtNS.Size = new System.Drawing.Size(100, 20);
+            this.txtNS.TabIndex = 9;
+            // 
+            // txtLuong
+            // 
+            this.txtLuong.Location = new System.Drawing.Point(62, 66);
+            this.txtLuong.Name = "txtLuong";
+            this.txtLuong.Size = new System.Drawing.Size(100, 20);
+            this.txtLuong.TabIndex = 11;
+            // 
+            // txtMaNGS
+            // 
+            this.txtMaNGS.Location = new System.Drawing.Point(249, 66);
+            this.txtMaNGS.Name = "txtMaNGS";
+            this.txtMaNGS.Size = new System.Drawing.Size(100, 20);
+            this.txtMaNGS.TabIndex = 12;
+            // 
+            // txtMaPB
+            // 
+            this.txtMaPB.Location = new System.Drawing.Point(436, 66);
+            this.txtMaPB.Name = "txtMaPB";
+            this.txtMaPB.Size = new System.Drawing.Size(100, 20);
+            this.txtMaPB.TabIndex = 13;
+            // 
+            // btnTHÊM
+            // 
+            this.btnTHÊM.Location = new System.Drawing.Point(342, 120);
+            this.btnTHÊM.Name = "btnTHÊM";
+            this.btnTHÊM.Size = new System.Drawing.Size(75, 23);
+            this.btnTHÊM.TabIndex = 14;
+            this.btnTHÊM.Text = "THÊM";
+            this.btnTHÊM.UseVisualStyleBackColor = true;
+            this.btnTHÊM.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvNhanVien
+            // 
+            this.dgvNhanVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNhanVien.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaNV,
+            this.HoTen,
+            this.NS,
+            this.GT});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvNhanVien.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvNhanVien.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(215)))), ((int)(((byte)(229)))));
+            this.dgvNhanVien.Location = new System.Drawing.Point(62, 190);
+            this.dgvNhanVien.Name = "dgvNhanVien";
+            this.dgvNhanVien.Size = new System.Drawing.Size(668, 148);
+            this.dgvNhanVien.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(568, 66);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Dự Án";
+            // 
+            // txtDuAn
+            // 
+            this.txtDuAn.Location = new System.Drawing.Point(611, 59);
+            this.txtDuAn.Name = "txtDuAn";
+            this.txtDuAn.Size = new System.Drawing.Size(100, 20);
+            this.txtDuAn.TabIndex = 18;
+            // 
+            // rabtn_nam
+            // 
+            this.rabtn_nam.AutoSize = true;
+            this.rabtn_nam.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.rabtn_nam.Location = new System.Drawing.Point(571, 17);
+            this.rabtn_nam.Name = "rabtn_nam";
+            this.rabtn_nam.Size = new System.Drawing.Size(59, 20);
+            this.rabtn_nam.TabIndex = 20;
+            this.rabtn_nam.TabStop = true;
+            this.rabtn_nam.Text = "Nam";
+            this.rabtn_nam.UseVisualStyleBackColor = true;
+            // 
+            // rabtn_nu
+            // 
+            this.rabtn_nu.AutoSize = true;
+            this.rabtn_nu.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.rabtn_nu.Location = new System.Drawing.Point(652, 19);
+            this.rabtn_nu.Name = "rabtn_nu";
+            this.rabtn_nu.Size = new System.Drawing.Size(46, 20);
+            this.rabtn_nu.TabIndex = 20;
+            this.rabtn_nu.TabStop = true;
+            this.rabtn_nu.Text = "Nữ";
+            this.rabtn_nu.UseVisualStyleBackColor = true;
+            // 
+            // MaNV
+            // 
+            this.MaNV.DataPropertyName = "MaNV";
+            this.MaNV.HeaderText = "MaNV";
+            this.MaNV.Name = "MaNV";
+            // 
+            // HoTen
+            // 
+            this.HoTen.DataPropertyName = "HoTen";
+            this.HoTen.HeaderText = "HoTen";
+            this.HoTen.Name = "HoTen";
+            // 
+            // NS
+            // 
+            this.NS.DataPropertyName = "NS";
+            this.NS.HeaderText = "NS";
+            this.NS.Name = "NS";
+            // 
+            // GT
+            // 
+            this.GT.DataPropertyName = "GT";
+            this.GT.HeaderText = "GT";
+            this.GT.Name = "GT";
+            // 
+            // Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 350);
+            this.Controls.Add(this.rabtn_nu);
+            this.Controls.Add(this.rabtn_nam);
+            this.Controls.Add(this.txtDuAn);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dgvNhanVien);
+            this.Controls.Add(this.btnTHÊM);
+            this.Controls.Add(this.txtMaPB);
+            this.Controls.Add(this.txtMaNGS);
+            this.Controls.Add(this.txtLuong);
+            this.Controls.Add(this.txtNS);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMaNV);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Add";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add";
+            this.Load += new System.EventHandler(this.Add_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNhanVien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtNS;
+        private System.Windows.Forms.TextBox txtLuong;
+        private System.Windows.Forms.TextBox txtMaNGS;
+        private System.Windows.Forms.TextBox txtMaPB;
+        private System.Windows.Forms.Button btnTHÊM;
+        private DevComponents.DotNetBar.Controls.DataGridViewX dgvNhanVien;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtDuAn;
+        private System.Windows.Forms.RadioButton rabtn_nam;
+        private System.Windows.Forms.RadioButton rabtn_nu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaNV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GT;
+    }
+}
---
+++ QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/GUI/Add.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAL;
+using BUS;
+
+
+namespace QuanLyNhanSu.GUI
+{
+    public partial class Add : Form
+    {
+        TaiKhoanBUS tkb = new TaiKhoanBUS();
+        public Add()
+        {
+            InitializeComponent();
+        }
+        QLTaiKhoanDataContext tks = new QLTaiKhoanDataContext();
+        private void Add_Load(object sender, EventArgs e)
+        {
+            dgvNhanVien.DataSource = tkb.getData();            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            QLTaiKhoanDataContext tk = new QLTaiKhoanDataContext();
+
+            NhanVien NV = new NhanVien();
+            PhanCong pc = new PhanCong();
+
+            NV.MaNV = txtMaNV.Text;
+            pc.MaNV = txtMaNV.Text;
+            pc.MaDA = txtDuAn.Text;
+            NV.HoTen = txtHoTen.Text;
+            NV.Luong = Convert.ToInt32(txtLuong.Text);
+            NV.MaNGS = txtMaNGS.Text;
+            NV.MaPB = txtMaPB.Text;
+            if(rabtn_nam.Checked == true)
+            {
+                NV.GT = false;
+            }
+            if(rabtn_nu.Checked == true)
+            {
+                NV.GT = true;
+            }        
+            NV.NS = Convert.ToDateTime(txtNS.Text);
+            tks.PhanCongs.InsertOnSubmit(pc);
+            tks.NhanViens.InsertOnSubmit(NV);
+            tks.SubmitChanges();
+            
+
+            txtMaNV.Text = "";
+            txtHoTen.Text = "";
+            txtNS.Text = "";
+            txtLuong.Text = "";
+            txtMaNGS.Text = "";
+            txtMaPB.Text = "";
+            dgvNhanVien.DataSource = tkb.getData();
+            dgvNhanVien.Refresh();
+            MessageBox.Show("Đã thêm thành công");
+        }
+    }
+    }
